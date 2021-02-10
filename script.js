ba0f3dfaae9aa92b3ee00b851467985e2ca2ff52
Soufiane Elharem <44909504+Selharem@users.noreply.github.com>
2021-02-10 01:52:47 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//
+const cards = document.querySelectorAll(".cards__single");
+
+function flipCard() {
+  this.classList.toggle("flip");
+}
+cards.forEach((card) => card.addEventListener("click", flipCard));
+
+
+

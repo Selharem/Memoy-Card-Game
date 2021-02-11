7a893c093d67f0eaa9e7280a8a7afd96fb6b1899
Soufiane Elharem <44909504+Selharem@users.noreply.github.com>
2021-02-11 18:36:41 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -7,11 +7,31 @@ cards.forEach((card) => card.addEventListener("click", flipCard));
 
 let  VerifyId = a => alert(a);
 
-var Fruits = ['avocado.svg','blueberries.svg','cabbage.svg','cabbage.svg','cherry.svg','olives.svg','parsley.svg','radishes.svg','raspberry.svg']
+var Fruits = ['avocado.svg','blueberries.svg','cabbage.svg','spring-onion.svg','cherry.svg','olives.svg','parsley.svg','radishes.svg','raspberry.svg']
+var choosenFruits = [];
+var choosenCards = [];
 
-for(var i=11,j=0;i<=99;i+=11,j++){
-  document.getElementById(i).src = Fruits[j];
 
+
+for(var i=0,j=0;i<=20;i++,j++){
+    var a = Math.floor(Math.random() * 8) + 0;
+    if( Occurence(choosenFruits,Fruits[a]) <= 1 ){
+      choosenFruits.push(Fruits[a]);
+      document.getElementById(i).src = Fruits[a];
+    }
+}
+
+
+console.log(choosenFruits);
+
+function Occurence(Array1,char) {
+    var result = 0;
+    for(var k=0;k<Array1.length;k++){
+        if(Array1[k]==char){
+            result ++;
+        }
+    }
+    return result;
 }
 
 

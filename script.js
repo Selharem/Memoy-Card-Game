ebc9f59d4f81c557f8b20485b1b81ed9be68355b
Soufiane Elharem <44909504+Selharem@users.noreply.github.com>
2021-02-13 02:09:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,49 @@
-const cards = document.querySelectorAll(".cards__single");
-
-function flipCard() {
-  this.classList.toggle("flip");
-}
-cards.forEach((card) => card.addEventListener("click", flipCard));
-
-let  VerifyId = a => alert(a);
-
-var Fruits = ['avocado.svg','blueberries.svg','cabbage.svg','spring-onion.svg','cherry.svg','olives.svg','parsley.svg','radishes.svg','raspberry.svg']
+var Fruits = ['avocado.svg','blueberries.svg','cabbage.svg','spring-onion.svg','cherry.svg','olives.svg','parsley.svg','radishes.svg','raspberry.svg'];
 var choosenFruits = [];
 var choosenCards = [];
+flippa();
+fakeRandom();
+//let  VerifyId = a => alert(a);
+console.log(choosenFruits);
 
 
+function flippa(){
+  const cards = document.querySelectorAll(".cards__single");
+  function flipCard() {
+    this.classList.toggle("flip");
+  }
+  cards.forEach((card) => card.addEventListener("click", flipCard));
+}
 
-for(var i=0,j=0;i<=20;i++,j++){
-    var a = Math.floor(Math.random() * 8) + 0;
+function fakeRandom(){ //just for testing 
+  var l = 0;
+  for(var i=0,j=1;i<=18;i+=2,j+=2){
+    if(l==8){
+      l=0;
+    }
+    document.getElementById(i).src = Fruits[l];
+    document.getElementById(j).src = Fruits[l];
+    l++;
+
+  }
+}
+
+function chooseRandom(){
+  for(var i=0,j=0;i<=17;i++,j++){
+    var a = Math.floor(Math.random() * 9) + 0;
     if( Occurence(choosenFruits,Fruits[a]) <= 1 ){
       choosenFruits.push(Fruits[a]);
       document.getElementById(i).src = Fruits[a];
     }
+  }
 }
-
-
-console.log(choosenFruits);
 
 function Occurence(Array1,char) {
-    var result = 0;
-    for(var k=0;k<Array1.length;k++){
-        if(Array1[k]==char){
-            result ++;
-        }
+  var result = 0;
+  for(var k=0;k<Array1.length;k++){
+    if(Array1[k]==char){
+        result ++;
     }
-    return result;
+  }
+  return result;
 }
-
-

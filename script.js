95bc52b32a4b17f02a44e7dfc12e52896be03fd7
Soufiane Elharem <44909504+Selharem@users.noreply.github.com>
2021-02-16 22:19:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ var choosenCards = [];
 var id_clicked1 = null;
 var id_clicked2 = null;
 var clicked_turn = 0;
-flippa();
+var clicked = ['back','back','back','back','back','back','back','back','back','back','back','back','back','back','back','back','back','back','back','back','back','back','back','back','back','back','back'];
+
+//flippa();
 fakeRandom();
 
 function VerifyId(a) {
+  flippa(a);
   if(clicked_turn == 0){
     id_clicked1 = a;
     clicked_turn = 1;
@@ -19,13 +22,22 @@ function VerifyId(a) {
   }
 }
 
-function flippa(){
+
+function flippa(a) {
+  if(clicked[a-100] == 'back'){
+    clicked[a-100] = 'front'
+  var card = document.getElementById(a);
+  card.classList.toggle('flip');
+  }
+}
+
+/*function flippa2(){
   const cards = document.querySelectorAll(".cards__single");
-  function flipCard() {
+  function flipCard2() {
     this.classList.toggle("flip");
   }
   cards.forEach((card) => card.addEventListener("click", flipCard));
-}
+}*/
 
 function fakeRandom(){ //just for testing 
   var l = 0;
@@ -33,34 +45,28 @@ function fakeRandom(){ //just for testing
     if(l==9){
       l=0;
     }
-
-    
-
-    //document.getElementById(i).src = Fruits[l];
     choosenFruits[i] = Fruits[l];
-    //document.getElementById(j).src = Fruits[l];
     choosenFruits[j] = Fruits[l];
     document.getElementById('back' + i).src = 'Images/' + Fruits[l] + '.svg';
     document.getElementById('back' + j).src = 'Images/' + Fruits[l] + '.svg';
     l++;
-
   }
 }
 
-console.log(choosenFruits);
-
 function verifySimilarity(){
   if(id_clicked1 != null && id_clicked2 != null) {
-      if(choosenFruits[id_clicked1-100] == choosenFruits[id_clicked2-100]){
+      if(choosenFruits[id_clicked1-100] == choosenFruits[id_clicked2-100] && id_clicked1!=id_clicked2){
         alert('success');
         id_clicked1 = null;
         id_clicked2 = null;
         clicked_turn == 0;
+      } else{
+        alert('echec')
       }
   }
 }
 
-function chooseRandom(){
+/*function chooseRandom(){
   for(var i=0,j=0;i<=17;i++,j++){
     var a = Math.floor(Math.random() * 9) + 0;
     //if( Occurence(choosenFruits,Fruits[a]) <= 1 ){
@@ -79,4 +85,4 @@ function Occurence(Array1,char) {
     }
   }
   return result;
-}
+}*/
